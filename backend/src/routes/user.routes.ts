b50ce8d6d8e6293ee57c89628b99a1448c82399a
Franco Avanzatti <avanzatti5@gmail.com>
2026-06-16 16:33:52 -0300

    guardado de routeuser

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,23 +1,17 @@
 import { Router } from "express";
 import { User } from "../models/User";
-import { authMiddleware, requireRole } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.get(
-  "/",
-  authMiddleware,
-  requireRole("admin"),
-  async (_req, res, next) => {
-    try {
-      const users = await User.find()
-        .select("name email avatar role createdAt")
-        .sort({ createdAt: -1 });
-      res.json(users);
-    } catch (error) {
-      next(error);
-    }
+router.get("/", async (_req, res, next) => {
+  try {
+    const users = await User.find()
+      .select("name email avatar role createdAt")
+      .sort({ createdAt: -1 });
+    res.json(users);
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 export default router;
